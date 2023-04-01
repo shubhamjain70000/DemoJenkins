29f45f91886b5259640660762c5575a7fa8b9bdb
Shubham <shubhamjain02081996@gmail.com>
2023-04-01 10:31:44 +0530

    New Class Demo5

---
+++ src/test/java/PracticePackage/Demo5.java
@@ -0,0 +1,14 @@
+package PracticePackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo5 {
+	
+	@Test
+	public void Test_5()
+	{
+		Reporter.log("Run Test_5",true);
+	}
+
+}
